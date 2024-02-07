24a73c0e2c546639f9f445a0e82384bb0538bd07
MostNamesAreTaken <116924524+AsadPatwekar@users.noreply.github.com>
2024-02-06 22:08:51 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+var app = express();
+app.use(express.static('styles'));
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+app.get("", (req, res, next) => { 
+  // show the page
+     const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const _retfile = path.join(__dirname, 'index.html');
+ 
+  res.sendFile(_retfile);
+ });
